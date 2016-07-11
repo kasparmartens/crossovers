c62274b3e841aafb301c97412b0b0fe15bd8360b
kasparmartens <kaspar.martens@gmail.com>
2016-07-11 16:36:17 +0100

    non-Markov data

--- generate_data.R
+++ generate_data.R
@@ -13,3 +13,19 @@ generate_discrete_HMM = function(n, starting_probs, hidden_transition, obs_trans
   }
   return(list(x = factor(x, levels = 1:k_hidden), y = factor(y, levels = 1:k_obs)))
 }
+
+generate_nonmarkov_seq = function(n, obs_transition, n_breakpoints = 5){
+  x = rep(NA, n)
+  k_hidden = nrow(obs_transition)
+  breakpoints = c(sort(sample(1:(n-1), n_breakpoints)), n)
+  for(i in 1:length(breakpoints)){
+    if(i == 1) start = 1 else start = breakpoints[i-1]+1
+    x[start:breakpoints[i]] = sample(1:k_hidden, 1)
+  }
+  y = rep(NA, n)
+  k_obs = ncol(obs_transition)
+  for(i in 1:n){
+    y[i] = sample(1:k_obs, 1, prob = obs_transition[x[i], ])
+  }
+  return(list(x = factor(x, levels = 1:k_hidden), y = factor(y, levels = 1:k_obs)))
+}
